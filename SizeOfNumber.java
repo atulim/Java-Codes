0c177c3dd803e4a5511e0666d86b5ab7281ef727
atulim <atul.kaushikim@gmail.com>
2017-10-30 11:19:23 +0530

    Changed the name to SIZEOFNUMBER

--- SizeOfNumber.java
+++ SizeOfNumber.java
@@ -1,6 +1,6 @@
 import java.io.*;
 import java.util.*;
-class SizeOfNumber {
+class SIZEOFNUMBER {
  public static void main(String[] args) {
   Scanner sc = new Scanner(System.in);
   int t = sc.nextInt();
